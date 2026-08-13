88529d649cbc6eddb191c0e88aba7752568ed96e
Seyud <2159455958@qq.com>
2026-08-13 22:03:16 +0800

    opt(native): release 原生二进制改用 --strip-unneeded 剥离
    - Application.mk：APP_STRIP_MODE 由全局 none 下移到分支——debug
      维持 none 不剥离便于调试，release 改用 --strip-unneeded 仅剥离
      无用符号，缩小发布产物体积

--- native/src/Application.mk
+++ native/src/Application.mk
@@ -4,16 +4,17 @@ APP_CPPFLAGS     := -std=c++23
 APP_STL          := none
 APP_PLATFORM     := android-24
 APP_THIN_ARCHIVE := true
-APP_STRIP_MODE   := none
 
 ifdef MAGISK_DEBUG
 
+APP_STRIP_MODE   := none
 NDK_APP_OUT 	 := ./obj/debug
 APP_CFLAGS       += -flto=thin -gdwarf-4
 APP_LDFLAGS      += -flto=thin
 
 else
 
+APP_STRIP_MODE   := --strip-unneeded
 NDK_APP_OUT 	 := ./obj/release
 APP_CFLAGS       += -flto
 APP_LDFLAGS      += -flto -Wl,--icf=all
